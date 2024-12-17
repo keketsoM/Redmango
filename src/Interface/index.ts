9378d9b9c10b04aad1927d1bb8c514dcb681cba1
Keketso <kekekeketso03@gmail.com>
2024-12-18 00:09:26 +0200

    shoppingCartInterface

--- src/Interface/index.ts
+++ src/Interface/index.ts
@@ -1,3 +1,4 @@
 import menuItemModel from "./menuItemModule";
-
-export type {menuItemModel};
+import shoppingCartModel from "./shoppingCartModel";
+import cartItemModel from "./cartItemModel";
+export type {menuItemModel,shoppingCartModel,cartItemModel};
