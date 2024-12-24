736c683245d40ddb523d2117da89bfe71af38da2
Keketso <kekekeketso03@gmail.com>
2024-12-24 14:13:21 +0200

    Controller Components in Register

--- src/Pages/Register.tsx
+++ src/Pages/Register.tsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useState } from "react";
+import inputHelper from "../Helper/inputHelper";
+import { SD_Roles } from "../Utility/SD";
 
 function Register() {
+  const [loading, setLoading] = useState(false);
+  const [userInput, setUserInput] = useState({
+    userName: "",
+    password: "",
+    role: "",
+    name: "",
+  });
+  const handleUserInput = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const tempData = inputHelper(e, userInput);
+    setUserInput(tempData);
+  };
   return (
     <div className="container text-center">
       <form method="post">
@@ -12,6 +27,9 @@ function Register() {
               className="form-control"
               placeholder="Enter Username"
               required
+              name="userName"
+              value={userInput.userName}
+              onChange={handleUserInput}
             />
           </div>
           <div className="col-sm-6 offset-sm-3 col-xs-12 mt-4">
@@ -19,6 +37,9 @@ function Register() {
               type="text"
               className="form-control"
               placeholder="Enter Name"
+              name="name"
+              value={userInput.name}
+              onChange={handleUserInput}
               required
             />
           </div>
@@ -27,14 +48,23 @@ function Register() {
               type="password"
               className="form-control"
               placeholder="Enter Password"
+              name="password"
+              value={userInput.password}
+              onChange={handleUserInput}
               required
             />
           </div>
           <div className="col-sm-6 offset-sm-3 col-xs-12 mt-4">
-            <select className="form-control form-select" required>
+            <select
+              className="form-control form-select"
+              name="role"
+              value={userInput.role}
+              onChange={handleUserInput}
+              required
+            >
               <option value="">--Select Role--</option>
-              <option value="customer">Customer</option>
-              <option value="admin">Admin</option>
+              <option value={SD_Roles.CUSTOMER}>Customer</option>
+              <option value={SD_Roles.ADMIN}>Admin</option>
             </select>
           </div>
         </div>
@@ -45,7 +75,7 @@ function Register() {
         </div>
       </form>
     </div>
-  )
+  );
 }
 
-export default Register
+export default Register;
