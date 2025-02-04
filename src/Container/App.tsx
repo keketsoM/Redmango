c79f6b9e2a9dcc80100aebec4d82941b4ed4aa9e
Keketso <kekekeketso03@gmail.com>
2025-02-04 22:32:07 +0200

    fixRefreshBugAuth

--- src/Container/App.tsx
+++ src/Container/App.tsx
@@ -1,18 +1,28 @@
+import jwt_decode from "jwt-decode";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import { useGetAllShoppingCartQuery } from "../Apis/ShoppingCartApi";
 import { Footer, Header } from "../Components/Layout";
+import { userModel } from "../Interface";
 import { Home, Login, MenuItemDetails, NotFound, Register } from "../Pages";
 import ShoppingCart from "../Pages/ShoppingCart";
 import { setShoppingCart } from "../Storage/Redux/shoppingCartSlice";
-
+import { setLoggedInUser } from "../Storage/Redux/userAuthSlice";
 function App() {
   const dispatch = useDispatch();
   const { data, isLoading } = useGetAllShoppingCartQuery(
     "f3443504-018c-4d9d-beba-1bfebdc249a9"
   );
 
+  useEffect(() => {
+    const localToken = localStorage.getItem("token");
+    if (localToken) {
+      const { unique_name, nameid, email, role }: userModel =
+        jwt_decode(localToken);
+      dispatch(setLoggedInUser({ unique_name, nameid, email, role }));
+    }
+  }, []);
   useEffect(() => {
     if (!isLoading) {
       dispatch(setShoppingCart(data.result?.cartItems));
