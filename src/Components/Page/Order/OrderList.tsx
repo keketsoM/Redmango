ddc1f7189507e3664048501a5b393108c5d368b0
Keketso <kekekeketso03@gmail.com>
2025-02-16 11:33:24 +0200

    orderBadgeonOrderList

--- src/Components/Page/Order/OrderList.tsx
+++ src/Components/Page/Order/OrderList.tsx
@@ -1,9 +1,11 @@
 import { useNavigate } from "react-router-dom";
+import getStatusColour from "../../../Helper/getStatusColour";
 import OrderHeaderModel from "../../../Interface/OrderHeaderModel";
 import { MainLoader } from "../Common";
 import OrderListProps from "./OrderListType";
 function OrderList({ isLoading, orderData }: OrderListProps) {
   const navigate = useNavigate();
+
   return (
     <>
       {isLoading && <MainLoader />}
@@ -13,18 +15,20 @@ function OrderList({ isLoading, orderData }: OrderListProps) {
           <div className="p-2">
             <div className="row border">
               <div className="col-1">ID</div>
-              <div className="col-3">Name</div>
+              <div className="col-2">Name</div>
               <div className="col-2">Phone</div>
               <div className="col-1">Total</div>
               <div className="col-1">Items</div>
               <div className="col-2">Date</div>
-              <div className="col-2"></div>
+              <div className="col-2">Status</div>
+              <div className="col-1"></div>
             </div>
             {orderData.map((orderItem: OrderHeaderModel) => {
+              const badgeColor = getStatusColour(orderItem.status!);
               return (
                 <div className="row border" key={orderItem.orderHeaderId}>
                   <div className="col-1">{orderItem.orderHeaderId}</div>
-                  <div className="col-3">{orderItem.pickupName}</div>
+                  <div className="col-2">{orderItem.pickupName}</div>
                   <div className="col-2">{orderItem.pickupPhoneNumber}</div>
                   <div className="col-1">
                     $ {orderItem.orderTotal?.toFixed(2)}
@@ -34,6 +38,11 @@ function OrderList({ isLoading, orderData }: OrderListProps) {
                     {new Date(orderItem.orderDate!).toLocaleDateString()}
                   </div>
                   <div className="col-2">
+                    <span className={`badge bg-${badgeColor}`}>
+                      {orderItem.status}
+                    </span>
+                  </div>
+                  <div className="col-1">
                     <button
                       onClick={() =>
                         navigate(
--- src/Interface/OrderHeaderModel.ts
+++ src/Interface/OrderHeaderModel.ts
@@ -1,16 +1,17 @@
-import OrderDetail from "./OrderDetailModel"
+import { SD_Status } from "../Utility/SD";
+import OrderDetail from "./OrderDetailModel";
 
 export default interface OrderHeaderModel {
-    orderHeaderId?: number
-  pickupName?: string
-  pickupPhoneNumber?: string
-  pickupEmail?: string
-  applicationUserId?: string
-  applicationUser?: any
-  orderTotal?: number
-  orderDate?: string
-  status?: string
-  stripePaymentIntentID?: string
-  totalItems?: number
-  orderDetails?: OrderDetail[]
+  orderHeaderId?: number;
+  pickupName?: string;
+  pickupPhoneNumber?: string;
+  pickupEmail?: string;
+  applicationUserId?: string;
+  applicationUser?: any;
+  orderTotal?: number;
+  orderDate?: string;
+  status?: SD_Status;
+  stripePaymentIntentID?: string;
+  totalItems?: number;
+  orderDetails?: OrderDetail[];
 }
