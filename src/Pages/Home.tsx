29579ba5c02fa9b71843afa54aaa810136476c50
Keketso <kekekeketso03@gmail.com>
2025-02-27 21:07:03 +0200

    Banner

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,7 +1,9 @@
+import { Banner } from "../Components/Page/Common";
 import { MenuItemList } from "../Components/Page/MenuItem";
 function Home() {
   return (
     <div>
+      <Banner />
       <div className="container p-2">
         <MenuItemList />
       </div>
