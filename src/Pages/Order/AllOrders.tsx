9e777d0c35994d9fc536f01c84be4b481e35e136
Keketso <kekekeketso03@gmail.com>
2025-03-06 15:40:32 +0200

    AddUIAllOrderFilter

--- src/Pages/Order/AllOrders.tsx
+++ src/Pages/Order/AllOrders.tsx
@@ -1,22 +1,35 @@
-
 import { useGetAllOrderQuery } from "../../Apis/orderApi";
 import { MainLoader } from "../../Components/Page/Common";
 import OrderList from "../../Components/Page/Order/OrderList";
 
-
 import withAdminAuth from "../../HOC/withAdminAuth";
-function MyOrders() {
-
+function AllOrders() {
   const { data, isLoading } = useGetAllOrderQuery("");
   console.log(data);
   return (
     <>
       {isLoading && <MainLoader />}
       {!isLoading && (
-        <OrderList isLoading={isLoading} orderData={data.result} />
+        <>
+          <div className="d-flex align-items-center justify-content-between mx-5 mt-5">
+            <h1 className="text-success">Orders List</h1>
+            <div className="d-flex " style={{ width: "40%" }}>
+              <input
+                type="text"
+                className="form-control mx-2"
+                placeholder="Search Name, Email or Phone"
+              />
+              <select className="form-select w-50 mx-2">
+                <option value="All">All</option>
+              </select>
+              <button className="btn btn-outline-success">Filter</button>
+            </div>
+          </div>
+          <OrderList isLoading={isLoading} orderData={data.result} />
+        </>
       )}
     </>
   );
 }
 
-export default withAdminAuth(MyOrders);
+export default withAdminAuth(AllOrders);
--- src/Pages/Order/MyOrders.tsx
+++ src/Pages/Order/MyOrders.tsx
@@ -4,15 +4,31 @@ import { MainLoader } from "../../Components/Page/Common";
 import OrderList from "../../Components/Page/Order/OrderList";
 import withAuth from "../../HOC/withAuth";
 import { RootState } from "../../Storage/Redux/store";
+import { SD_Status } from "../../Utility/SD";
+
+const filterOption = [
+  "All",
+  SD_Status.CONFIRMED,
+  SD_Status.BEING_COOKING,
+  SD_Status.READY_FOR_PICKUP,
+  SD_Status.CANCELLED
+]
+
 function MyOrders() {
   const userId = useSelector((state: RootState) => state.userAuthstore.nameid);
   const { data, isLoading } = useGetAllOrderQuery(userId);
+  console.log(userId);
   console.log(data);
   return (
     <>
       {isLoading && <MainLoader />}
       {!isLoading && (
-        <OrderList isLoading={isLoading} orderData={data.result} />
+        <>
+          <div className="d-flex align-items-center justify-content-between mx-5 mt-5">
+            <h1 className="text-success">My Orders</h1>
+          </div>
+          <OrderList isLoading={isLoading} orderData={data.result} />
+        </>
       )}
     </>
   );
